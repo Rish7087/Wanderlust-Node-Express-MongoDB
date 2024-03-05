9cc15453571e924a1e054f1781d8c04da3d48877
Rishabh Saini <rishabhsaini40618@gmail.com>
2024-03-05 13:09:16 +0530

    Remove node_modules from tracking

--- routes/listing.js
+++ routes/listing.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const upload = multer({dest: 'uploads/'});
 
 router.route("/")
 .get(wrapAsync(listingController.index))
-.post(
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingController.createListing)
-);
+// .post(
+//   isLoggedIn,
+//   validateListing,
+//   wrapAsync(listingController.createListing)
+// );
+.post(upload.single("listing[image]"), (req, res) => {
+  res.send(req.file);
+})
 
 //new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
