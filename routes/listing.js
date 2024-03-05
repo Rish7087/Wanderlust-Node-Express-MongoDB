bf624787e40a74bfcd4f0665598cd17194860f02
Rishabh Saini <rishabhsaini40618@gmail.com>
2024-03-05 13:57:25 +0530

    modified:   app.js 	new file:   cloudConfig.js 	modified:   package-lock.json 	modified:   package.json 	modified:   routes/listing.js 	deleted:    uploads/52873acaba61402070f5fd8fc1b12c24

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,8 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer = require("multer");
-const upload = multer({dest: 'uploads/'});
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 router.route("/")
 .get(wrapAsync(listingController.index))
